1a8b3f90b2b1ea14d487e03095785d9c9aae80fa
Pablo Gómez <contact@pablo-gomez.net>
2022-10-06 11:00:58 +0200

    - Fixing flake8 config - Fixing long lines

--- paseos/paseos.py
+++ paseos/paseos.py
@@ -5,7 +5,8 @@ from .utils.load_default_cfg import load_default_cfg
 
 
 class PASEOS:
-    """This class serves as the main interface with the user. It is designed as a singleton to ensure we only have one instance running at any time."""
+    """This class serves as the main interface with the user. It is designed
+    as a singleton to ensure we only have one instance running at any time."""
 
     # Config file of the simulation
     # Beyond the initial cfg we use it to store variables
@@ -17,7 +18,7 @@ class PASEOS:
             self.instance = super(PASEOS, self).__new__(self)
         else:
             logger.warning(
-                "Tried to create another instance of PASEOS simulation. Keeping original one..."
+                "Tried to create another instance of PASEOS simulation.Keeping original one..."
             )
         return self.instance
 
