0277e1daf7f585e7f82ae95a5e9a39f2d2311c50
Pablo Gómez <contact@pablo-gomez.net>
2022-10-19 17:33:36 +0200

    Changed unit description for consistency

--- paseos/power/discharge_model.py
+++ paseos/power/discharge_model.py
@@ -3,14 +3,14 @@
 
 def discharge(
     actor,
-    power_consumption_in_wattseconds: float,
+    power_consumption_in_Ws: float,
     battery_model="simple",
 ):
     """Models the reduction of battery energy on the actor
 
     Args:
         actor (SpacecraftActor): Actor to apply this
-        power_consumption_in_wattseconds (float): Consumed power in Ws
+        power_consumption_in_Ws (float): Consumed power in Ws
         battery_model (str, optional): Defines which model to use, atm only "simple". Defaults to "simple".
 
     Returns:
@@ -18,17 +18,17 @@ def discharge(
     """
 
     if battery_model == "simple":
-        if actor.battery_level_in_Ws < power_consumption_in_wattseconds:
+        if actor.battery_level_in_Ws < power_consumption_in_Ws:
             raise ValueError(
                 "Insufficient Battery. Actor "
                 + str(actor)
                 + " has battery level: "
                 + str(actor.battery_level_in_Ws)
                 + " - trying to discharge "
-                + str(power_consumption_in_wattseconds)
+                + str(power_consumption_in_Ws)
             )
         else:
-            actor.battery_level_in_Ws -= power_consumption_in_wattseconds
+            actor.battery_level_in_Ws -= power_consumption_in_Ws
             return actor
     else:
         raise NotImplementedError("Unknown battery model " + battery_model)
