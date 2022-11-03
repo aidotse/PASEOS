49a21eb2dcb00a70bea620fcaf53b51034c2f6a0
johanos1 <oshtman@gmail.com>
2022-11-03 15:29:36 +0100

    Animation fine tuning

--- paseos/visualization/space_animation.py
+++ paseos/visualization/space_animation.py
@@ -5,6 +5,9 @@ from typing import List
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.artist import Artist
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from loguru import logger
 
 from paseos.actors.base_actor import BaseActor
@@ -17,8 +20,13 @@ from paseos.visualization.animation import Animation
 class SpaceAnimation(Animation):
     """This class visualizes the central body, local actor and known actors over time."""
 
-    def __init__(self, sim: PASEOS) -> None:
+    def __init__(self, sim: PASEOS, n_trajectory: int = 10) -> None:
+        """Initialize the space animation object
 
+        Args:
+            sim (PASEOS): simulation object
+            n_trajectory (int): number of samples in tail of actor
+        """
         super().__init__(sim)
         logger.trace("Initializing animation")
         # Create list of objects to be plotted
@@ -31,52 +39,97 @@ class SpaceAnimation(Animation):
             pos_norm = [x / self._norm_coeff for x in pos]
             self.objects.append(DotMap(actor=known_actor, positions=np.array(pos_norm)))
 
-        # create figure
-        self.fig = plt.figure(figsize=plt.figaspect(0.5) * 1.5)
-        self.ax = plt.subplot(projection="3d")
-        self.ax.set_title(self._sec_to_ddhhmmss(local_time_d / pk.SEC2DAY))
-        self.ax.get_xaxis().set_ticks([])
-        self.ax.get_yaxis().set_ticks([])
-        self.ax.get_zaxis().set_ticks([])
-        self.n_trajectory = 50  # how many samples from histories to visualize
-        self._textbox_offset = 0.25  # how many samples from histories to visualize
+        # create figures
+        # create grid for different subplots
+        spec = gridspec.GridSpec(
+            ncols=2,
+            nrows=1,
+            width_ratios=[2.5, 1],
+            wspace=0.5,
+            hspace=0.5,
+            height_ratios=[1],
+        )
+
+        # Create figure for 3d animation
+        self.fig = plt.figure(figsize=plt.figaspect(0.5) * 2)
+        self.ax_3d = plt.subplot(spec[0], projection="3d")
+        self.ax_3d.set_title(self._sec_to_ddhhmmss(local_time_d / pk.SEC2DAY))
+        self.ax_3d.get_xaxis().set_ticks([])
+        self.ax_3d.get_yaxis().set_ticks([])
+        self.ax_3d.get_zaxis().set_ticks([])
+        self.n_trajectory = n_trajectory  # how many samples from histories to visualize
+        self._textbox_offset = 0.1  # how many samples from histories to visualize
+
+        # Create figure for LOS
+        self.ax_los = plt.subplot(spec[1])
+        xaxis = np.arange(len(current_actors))
+        self.ax_los.set_xticks(xaxis)
+        self.ax_los.set_yticks(xaxis)
+        self.ax_los.set_xticklabels(current_actors)
+        self.ax_los.set_yticklabels(current_actors)
+        self.ax_los.figure.set_size_inches(10, 10)
 
         # plot the objects
         self._plot_central_body()
         self._plot_actors()
+        los_matrix = self._get_los_matrix(current_actors)
+        self._plot_los(los_matrix)
 
-        plt.show(block=False)
+        plt.ion()
+        plt.show()
 
     def _plot_central_body(self) -> None:
-        """Plot the central object as a sphere of radius 1
-        Args:
-            None
-        Returns:
-            None
-        """
+        """Plot the central object as a sphere of radius 1"""
         central_body = self._local_actor._central_body
         central_body.radius
 
-        u, v = np.mgrid[0:2 * np.pi:30j, 0:np.pi:20j]
+        u, v = np.mgrid[0 : 2 * np.pi : 30j, 0 : np.pi : 20j]
         x = np.cos(u) * np.sin(v)
         y = np.sin(u) * np.sin(v)
         z = np.cos(v)
-        self.ax.plot_surface(x, y, z, color="grey")
+        self.ax_3d.plot_surface(x, y, z, color="grey", alpha=0.4)
+
+    def _get_los_matrix(self, current_actors: List[BaseActor]) -> np.ndarray:
+        """Compute line-of-sight (LOS) between all actors
+
+        Args:
+            current_actors (List[BaseActor]): All actors in the simulation
+
+        Returns:
+            np.ndarray: LOS matrix with nonzero elements if actors are in LOS
+        """
+        local_time = self._local_actor.local_time
+        los_matrix = np.identity(len(current_actors))
+
+        for i, a1 in enumerate(current_actors):
+            for j, a2 in enumerate(current_actors[i + 1 :]):
+                if a1.is_in_line_of_sight(a2, local_time) is True:
+                    los_matrix[i, j + i + 1] = 1.0
+
+        # make los_matrix symmetric with diagonal entries equal to 0.5 to make colorbar nicer
+        los_matrix = los_matrix + los_matrix.T - 1.5 * np.diag(np.diag(los_matrix))
+        return los_matrix
 
     def _populate_textbox(self, actor: BaseActor) -> str:
         """Extracts information from an actor and builds a string
+
         Args:
             actor (BaseActor): an actor
+
         Returns:
             str: text to populate the textbox.
         """
+        info_str = f"{actor.name}"
         if isinstance(actor, SpacecraftActor):
             # TODO: enable both text and icons in textbox
-            battery_level = actor.battery_level_ratio * 100
-            info_str = f"{actor.name} \nbat: {battery_level:.0f}%"
+
+            if actor.battery_level_in_Ws is not None:
+                battery_level = actor.battery_level_ratio * 100
+                info_str += f"\nBattery: {battery_level:.0f}%"
+
             for name in actor.communication_devices.keys():
                 info = actor.communication_devices[name]
-                info_str += f"\n{name}: {info.bandwidth_in_kbps} kbps"
+                info_str += f"\nCommDevice1: {info.bandwidth_in_kbps} kbps"
 
         elif isinstance(actor, GroundstationActor):
             # TODO: implement textbox for groundstation
@@ -84,13 +137,25 @@ class SpaceAnimation(Animation):
 
         return info_str
 
-    def _plot_actors(self) -> None:
-        """Plots all the actors
+    def _plot_los(self, los_matrix: np.ndarray) -> None:
+        """Plot the LOS links in a heat map
+
         Args:
-            None
-        Returns:
-            None
+            los_matrix (np.ndarray): matrix telling what satellites can see each other.
         """
+        # Create new colormap, with white for two
+        colors = [(255, 0, 0), (255, 255, 255), (0, 255, 0)]
+        new_map = LinearSegmentedColormap.from_list("new_map", colors, N=3)
+        self._los_plot = self.ax_los.matshow(los_matrix, cmap=new_map, vmin=0, vmax=1)
+
+        divider = make_axes_locatable(self.ax_los)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+
+        cbar = self.fig.colorbar(self._los_plot, ticks=[0, 1], cax=cax)
+        cbar.ax.set_yticklabels(["no signal", "signal"])
+
+    def _plot_actors(self) -> None:
+        """Plots all the actors"""
         for obj in self.objects:
             data = obj.positions
             if data.ndim == 1:
@@ -117,9 +182,11 @@ class SpaceAnimation(Animation):
                     pass
             else:
                 if isinstance(obj.actor, SpacecraftActor):
-                    trajectory = self.ax.plot3D(data[0, 0], data[0, 1], data[0, 2])[0]
+                    trajectory = self.ax_3d.plot3D(data[0, 0], data[0, 1], data[0, 2])[
+                        0
+                    ]
                     obj.plot.trajectory = trajectory
-                    obj.plot.point = self.ax.plot(
+                    obj.plot.point = self.ax_3d.plot(
                         data[0, 0],
                         data[0, 1],
                         data[0, 2],
@@ -127,34 +194,48 @@ class SpaceAnimation(Animation):
                         color=trajectory.get_color(),
                     )[0]
                     actor_info = self._populate_textbox(obj.actor)
-                    obj.plot.text = self.ax.text(
-                        data[0, 0] + self._textbox_offset,
-                        data[0, 1] + self._textbox_offset,
-                        data[0, 2] + self._textbox_offset,
-                        actor_info,
-                        bbox=dict(facecolor="white"),
-                        verticalalignment="bottom",
-                        clip_on=True,
-                    )
+                    if obj.actor == self._local_actor:
+                        obj.plot.text = self.ax_3d.text(
+                            data[0, 0] + self._textbox_offset,
+                            data[0, 1] + self._textbox_offset,
+                            data[0, 2] + self._textbox_offset,
+                            actor_info,
+                            bbox=dict(facecolor="mediumspringgreen"),
+                            verticalalignment="bottom",
+                            clip_on=True,
+                        )
+
+                    else:
+                        obj.plot.text = self.ax_3d.text(
+                            data[0, 0] + self._textbox_offset,
+                            data[0, 1] + self._textbox_offset,
+                            data[0, 2] + self._textbox_offset,
+                            actor_info,
+                            bbox=dict(facecolor="white"),
+                            verticalalignment="bottom",
+                            clip_on=True,
+                        )
 
                 elif isinstance(obj.actor, GroundstationActor):
                     # TODO: implement initial rendering of groundstation object
                     pass
 
-        self.ax.set_box_aspect(
+        self.ax_3d.set_box_aspect(
             (
-                np.ptp(self.ax.get_xlim()),
-                np.ptp(self.ax.get_ylim()),
-                np.ptp(self.ax.get_zlim()),
+                np.ptp(self.ax_3d.get_xlim()),
+                np.ptp(self.ax_3d.get_ylim()),
+                np.ptp(self.ax_3d.get_zlim()),
             )
         )
 
     def _make_actor_list(self, sim: PASEOS) -> List[BaseActor]:
         """Arranges all actors (including the local) into a list
+
         Args:
-        sim: simulation object.
+            sim (PASEOS): simulation object.
+
         Returns:
-            List[BaseActor]
+            List[BaseActor]: list of all the actors known to local actor (including itself)
         """
         if len(sim.known_actors) > 0:
             current_actors = [sim.local_actor] + list(sim.known_actors.values())
@@ -164,12 +245,11 @@ class SpaceAnimation(Animation):
 
     def _update(self, sim: PASEOS) -> None:
         """Updates the animation with the current actor information
+
         Args:
-        sim (PASEOS): simulation object.
-        Returns:
-            None
+            sim (PASEOS): simulation object.
         """
-        logger.debug("Updating animation")
+        logger.trace("Updating animation")
         local_time_d = self._local_actor.local_time.mjd2000
         # NOTE: the actors in sim are unique so make use of sets
         objects_in_plot = set([obj.actor for obj in self.objects])
@@ -202,68 +282,86 @@ class SpaceAnimation(Animation):
                     pos, _ = known_actor.get_position_velocity(pk.epoch(local_time_d))
                     pos_norm = [x / self._norm_coeff for x in pos]
                     if "positions" in obj:
-                        obj.positions = np.vstack((obj.positions, pos_norm))
+                        if obj.positions.shape[0] > self.n_trajectory:
+                            obj.positions = np.roll(obj.positions, shift=-1, axis=0)
+                            obj.positions[-1, :] = pos_norm
+                        else:
+                            obj.positions = np.vstack((obj.positions, pos_norm))
                     else:
                         obj.positions = np.array(pos_norm)
         self._plot_actors()
 
         # Step through trajectories to find max and min values in each direction
-        coords_max = [0, 0, 0]
-        coords_min = [0, 0, 0]
+        coords_max = [
+            self.ax_3d.get_xlim()[1],
+            self.ax_3d.get_ylim()[1],
+            self.ax_3d.get_zlim()[1],
+        ]
+        coords_min = [
+            self.ax_3d.get_xlim()[0],
+            self.ax_3d.get_ylim()[0],
+            self.ax_3d.get_zlim()[0],
+        ]
         for obj in self.objects:
             coords_max = np.maximum(obj.positions.max(axis=0), coords_max)
             coords_min = np.minimum(obj.positions.min(axis=0), coords_min)
-        self.ax.set_xlim(coords_min[0], coords_max[0])
-        self.ax.set_ylim(coords_min[1], coords_max[1])
-        self.ax.set_zlim(coords_min[2], coords_max[2])
+        self.ax_3d.set_xlim(coords_min[0], coords_max[0])
+        self.ax_3d.set_ylim(coords_min[1], coords_max[1])
+        self.ax_3d.set_zlim(coords_min[2], coords_max[2])
 
-        self.ax.set_title(self._sec_to_ddhhmmss(local_time_d / pk.SEC2DAY))
+        # Update LOS heatmap
+        current_actors = list(current_actors)
+        los_matrix = self._get_los_matrix(current_actors)
+        self._los_plot.set_data(los_matrix)
+        xaxis = np.arange(len(current_actors))
+        self.ax_los.set_xticks(xaxis)
+        self.ax_los.set_yticks(xaxis)
+        self.ax_los.set_xticklabels(current_actors)
+        self.ax_los.set_yticklabels(current_actors)
+        self.fig.canvas.draw_idle()
 
-        return
+        try:
+            self.ax_3d.set_title(self._local_actor.local_time)
+        except:
+            pass
 
     def _animate(self, sim: PASEOS, dt: float) -> List[Artist]:
         """Advances the time of sim, updates the plot, and returns the axis objects
+
         Args:
-        sim (PASEOS): simulation object.
-        dt: size of time step
+            sim (PASEOS): simulation object.
+            dt (float): size of time step
+
         Returns:
             List[Artist]: list of Artist objects
         """
         sim.advance_time(dt)
         self._update(sim)
-        return self.ax.get_children()
+        return self.ax_3d.get_children() + self.ax_los.get_children()
 
     def _animation_wrapper(self, step: int, sim: PASEOS, dt: float) -> List[Artist]:
-        """wrapper to allow for frame numbers from animation
+        """Wrapper to allow for frame numbers from animation
+
         Args:
-        steps: current frame number of the animation
-        sim (PASEOS): simulation object.
-        dt: size of time step
+            step (int): current frame number of the animation
+            sim (PASEOS): simulation object.
+            dt (float): size of time step
+
         Returns:
             List[Artist]: list of Artist objects
         """
         return self._animate(sim, dt)
 
-    def animate(
-        self,
-        sim: PASEOS,
-        dt: float,
-        steps: int = 1,
-        name: str = None,
-        save_file: bool = False,
-    ) -> None:
+    def animate(self, sim: PASEOS, dt: float, steps: int = 1, name: str = None) -> None:
         """Animates paseos for a given number of steps with dt in each step.
-        If save=True, result is saved into "{name}.mp4"
+
         Args:
-        sim (PASEOS): simulation object.
-        dt(float): size of time step
-        steps (int): number of steps to animate
-        name (str): filename to save animation
-        save_file (bool): should animation be saved
-        Returns:
-            None
+            sim (PASEOS): simulation object.
+            dt (float): size of time step
+            steps (int, optional): number of steps to animate. Defaults to 1.
+            name (str, optional): filename to save the animation. Defaults to None.
         """
-        if save_file is False:
+        if name is None:
             self._animate(sim, dt)
         else:
             anim = animation.FuncAnimation(
@@ -276,5 +374,5 @@ class SpaceAnimation(Animation):
                 ),
                 interval=20,
                 blit=True,
-            )
+            )  # blit means to only redraw parts that changed
             anim.save(f"{name}.mp4", writer="ffmpeg", fps=10)
