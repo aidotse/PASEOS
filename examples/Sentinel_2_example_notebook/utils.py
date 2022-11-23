5cb8ec2a0c4c81f07644d528a85c0fb16ffdf58c
Gabriele.Meoni <Gabriele.Meoni@esa.int>
2022-11-23 12:37:54 +0100

    Adding GPL lincese.

--- examples/Sentinel_2_example_notebook/utils.py
+++ examples/Sentinel_2_example_notebook/utils.py
@@ -5,7 +5,18 @@ import rasterio
 # [1] Massimetti, Francesco, et al. ""Volcanic hot-spot detection using SENTINEL-2:
 # a comparison with MODIS–MIROVA thermal data series."" Remote Sensing 12.5 (2020):820."
 # The code of the function "s2pix_detector" and its subfunctions was taken and reimplemented by using numpy
-# from the project "sentinel2_l0" dataset, which will be released soon
+# from the project "sentinel2_l0" dataset, which will be released soon.
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
 
 
 def acquire_data(file_name):
