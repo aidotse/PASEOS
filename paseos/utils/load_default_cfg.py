b6546cee2d5976396ecf290507a1eb8d241f5513
johanos1 <oshtman@gmail.com>
2022-11-28 10:52:15 +0100

    Removed images from module.

--- paseos/utils/load_default_cfg.py
+++ paseos/utils/load_default_cfg.py
@@ -6,12 +6,11 @@ from loguru import logger
 
 def load_default_cfg():
     """Loads the default toml config file from the cfg folder."""
-    logger.debug("Loading default cfg...")
 
     path = os.path.join(
         os.path.dirname(__file__) + "/../resources/", "default_cfg.toml"
     )
-    logger.debug(f"loading path: {path}")
+    logger.debug(f"loading default cfg from path: {path}")
 
     with open(path) as cfg:
         # dynamic=False inhibits automatic generation of non-existing keys
