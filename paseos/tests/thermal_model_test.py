7db7758d651267da5d6f61ccf59e34ba63b09b1a
Pablo Gómez <contact@pablo-gomez.net>
2022-12-19 13:26:58 +0100

    first draft of a test

--- paseos/tests/thermal_model_test.py
+++ paseos/tests/thermal_model_test.py
@@ -1,7 +1,8 @@
 """Simple test of the thermal model to see if temperatures evolve as expected"""
-from test_utils import get_default_instance
+import pykep as pk
 
-from paseos import SpacecraftActor
+import paseos
+from paseos import SpacecraftActor, ActorBuilder, load_default_cfg
 import asyncio
 import pytest
 
@@ -13,117 +14,52 @@ async def wait_for_activity(sim):
 
 # tell pytest to create an event loop and execute the tests using the event loop
 @pytest.mark.asyncio
-async def test_activity():
-    """Test if performing activity consumes power as expected"""
-    sim, sat1, _ = get_default_instance()
+async def test_thermal():
+    """Test if performing activity changes temperature as expected"""
 
-    # Initial power is 500
-    assert sat1.battery_level_in_Ws == 500
+    # Define central body
+    earth = pk.planet.jpl_lp("earth")
 
-    # Out test case is a function that increments a value, genius.
-    # (needs a list to increase the actual value by reference and not create a copy)
-    test_val = [0]
+    # Define local actor
+    sat1 = ActorBuilder.get_actor_scaffold("sat1", SpacecraftActor, pk.epoch(0))
+    ActorBuilder.set_orbit(sat1, [10000000, 0, 0], [0, 8000.0, 0], pk.epoch(0), earth)
+    ActorBuilder.set_power_devices(sat1, 500, 10000, 1)
+    ActorBuilder.set_thermal_model(
+        actor=sat1,
+        actor_mass=50.0,
+        actor_initial_temperature_in_K=273.15,
+        actor_sun_absorptance=1.0,
+        actor_infrared_absorptance=1.0,
+        actor_sun_facing_area=1.0,
+        actor_central_body_facing_area=1.0,
+        actor_emissive_area=1.0,
+        actor_thermal_capacity=1000,
+    )
+
+    # init simulation
+    cfg = load_default_cfg()  # loading cfg to modify defaults
+    cfg.sim.dt = 0.1  # setting lower timestep to run things quickly
+    cfg.sim.activity_timestep = 0.1
+    cfg.io.logging_interval = 0.25  # log every 0.25 seconds
+    cfg.sim.time_multiplier = 50  # speed up execution for convenience
+    sim = paseos.init_sim(sat1, cfg)
+
+    # Initial temperature is 0C / 273.15K
+    assert sat1.temperature_in_K == 273.15
 
     async def func(args):
-        for _ in range(10):
-            args[0][0] += 1
-            await asyncio.sleep(0.2)
+        await asyncio.sleep(3.0)
 
     # Register an activity that draws 10 watt per second
     sim.register_activity(
-        "Testing", activity_function=func, power_consumption_in_watt=10
+        "Activity_1", activity_function=func, power_consumption_in_watt=10
     )
 
     # Run the activity
-    sim.perform_activity("Testing", activity_func_args=[test_val])
+    sim.perform_activity("Activity_1")
     await wait_for_activity(sim)
-
-    # Check activity result
-    assert test_val[0] == 10
-
-    # Check power was depleted as expected
-    # Activity should run roughly 2s
-    # We charge 1W per second
-    # And discharge 10W per second
-    # So should be roughly 20W - 6W consumed from starting 500
-    assert sat1.battery_level_in_Ws > 480 and sat1.battery_level_in_Ws < 490
+    raise (1)
 
 
-@pytest.mark.asyncio
-async def test_running_two_activities():
-    """This test ensures that you cannot run two activities at the same time."""
-    sim, sat1, earth = get_default_instance()
-
-    # Declare two activities, one calls the other
-    test_value = [0]
-
-    async def act1(args):
-        args[0][0] = 42  # this gets executed
-        sim.perform_activity("act2")
-        await asyncio.sleep(0.1)
-
-    async def act2(args):
-        args[0][0] = -1  # this won't
-        await asyncio.sleep(0.1)
-
-    # Register both activities
-    sim.register_activity("act1", activity_function=act1, power_consumption_in_watt=10)
-    sim.register_activity("act2", activity_function=act2, power_consumption_in_watt=10)
-
-    # try running it
-    sim.perform_activity("act1", activity_func_args=[test_value])
-    await wait_for_activity(sim)
-
-    # Value should be 42 as first activity is started but then an error occurs trying the second
-    assert test_value[0] == 42
-
-
-@pytest.mark.asyncio
-async def test_activity_constraints():
-    """Tests if creating a constraint function to be satisfied during an activity works.
-    Here we start a function that counts up until we stop charging our solar panels and then prints the value.
-    """
-
-    sim, sat1, earth = get_default_instance()
-    assert sat1.battery_level_in_Ws == 500
-
-    # Out test case is a function that increments a value, genius.
-    # (needs a list so increase the actual value by reference and don't create a copy)
-    test_value = [0]
-    test_value2 = [-1]
-
-    async def func(args):
-        while True:
-            args[0][0] += 1
-            await asyncio.sleep(1.0)
-
-    # Constraint that becomes false once the actor has charge it's battery over 510
-    async def constraint(args):
-        local_actor: SpacecraftActor = args[0]
-        return local_actor.battery_level_in_Ws < 505
-
-    # On termination print the value and make test_value2 the same.
-    async def on_termination(args):
-        # print(args)
-        args[1][0] = args[0][0]
-
-    # Register an activity that draws no power per second
-    sim.register_activity(
-        "Testing constraints",
-        activity_function=func,
-        power_consumption_in_watt=0,
-        on_termination_function=on_termination,
-        constraint_function=constraint,
-    )
-
-    # Run the activity
-    sim.perform_activity(
-        "Testing constraints",
-        activity_func_args=[test_value],
-        constraint_func_args=[sat1],
-        termination_func_args=[test_value, test_value2],
-    )
-    await wait_for_activity(sim)
-
-    assert test_value == test_value2
-    assert sat1.battery_level_in_Ws >= 505
+if __name__ == "__main__":
+    test_thermal()
