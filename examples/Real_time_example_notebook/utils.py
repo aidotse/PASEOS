d4efabefd45ed72a2641f52b03abebf0503b5704
Gabriele Meoni <Gabriele.Meoni@esa.int>
2023-01-30 19:12:57 +0100

    Fixing small bug.

--- examples/Real_time_example_notebook/utils.py
+++ examples/Real_time_example_notebook/utils.py
@@ -24,7 +24,7 @@ def extract_cpu_time_from_file(file_name):
     Returns:
         dict: {computing_part_name : cpu_time}
     """
-    task_name_list=["detect_volcanic_eruptions_async", "PASEOS.advance_time", "check_los_with_ground_station", "ThermalModel.update_temperature", "RadiationModel.did_device_restart", "..odel.did_device_experience_failure"]
+    task_name_list=["PASEOS.perform_activity", "detect_volcanic_eruptions_async", "PASEOS.advance_time", "check_los_with_ground_station", "ThermalModel.update_temperature", "RadiationModel.did_device_restart", "..odel.did_device_experience_failure"]
 
     f=open(file_name,"r")
     profiling_results_text=f.read()
@@ -48,11 +48,13 @@ def extract_cpu_time_from_file(file_name):
                     task_to_track_idx_list.append(idx)
                     break
 
-    task_name_id_dict=dict(zip(task_name_list, task_to_track_idx_list))
+    task_name_list_sorted=[profiling_results[idx][-5] for idx in  task_to_track_idx_list]
+    task_name_id_dict=dict(zip(task_name_list_sorted, task_to_track_idx_list))
+    total_time=float(profiling_results[task_name_id_dict["PASEOS.perform_activity"]][-2])
     time_activity=float(profiling_results[task_name_id_dict["detect_volcanic_eruptions_async"]][-2])
     paseos_activity=float(profiling_results[task_name_id_dict["PASEOS.advance_time"]][-2])
     los_check=float(profiling_results[task_name_id_dict["check_los_with_ground_station"]][-2])
     thermal_model=float(profiling_results[task_name_id_dict["ThermalModel.update_temperature"]][-2])/2
     charge_model=thermal_model
     radiation_time=float(profiling_results[task_name_id_dict["RadiationModel.did_device_restart"]][-2])+float(profiling_results[task_name_id_dict["..odel.did_device_experience_failure"]][-2])
-    return dict(zip(["activity", "paseos", "charge", "radiation", "thermal", "los_check"],[time_activity, paseos_activity, charge_model, radiation_time, thermal_model, los_check]))
+    return dict(zip(["total_time","activity", "paseos", "charge", "radiation", "thermal", "los_check"],[total_time, time_activity, paseos_activity, charge_model, radiation_time, thermal_model, los_check]))
