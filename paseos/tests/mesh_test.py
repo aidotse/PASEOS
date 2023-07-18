203d10a25b47c4edc75e532f4e120e64d42b7725
Pablo Gómez <contact@pablo-gomez.net>
2023-07-18 13:46:54 +0200

    Some fixes

--- paseos/tests/mesh_test.py
+++ paseos/tests/mesh_test.py
@@ -21,7 +21,7 @@ M = 73.57 * pk.DEG2RAD
 # We overestimate MU due to a bug in pykep
 # https://github.com/esa/pykep/issues/167
 # MU = 666.19868
-MU = 2e4
+MU = 1e5
 
 
 def test_mesh_for_central_body():
@@ -30,12 +30,12 @@ def test_mesh_for_central_body():
     paseos.set_log_level("TRACE")
 
     # Create a planet object from pykep for 67P
-    comet = pk.planet.keplerian(epoch, (a, e, i, W, w, M), pk.MU_SUN, MU, 4000, 4000, "67P")
+    comet = pk.planet.keplerian(epoch, (a, e, i, W, w, M), pk.MU_SUN, MU, 2000, 2000, "67P")
 
     # Load the 67P mesh with pickle
     with open(mesh_path, "rb") as f:
         mesh_points, mesh_triangles = pickle.load(f)
-        mesh_points = np.array(mesh_points)
+        mesh_points = np.array(mesh_points) * 3126.6064453124995  # Scale to m
         mesh_triangles = np.array(mesh_triangles)
 
     # Define local actor
@@ -43,22 +43,30 @@ def test_mesh_for_central_body():
 
     # Set a keplerian orbit around 67P
     # Note that keplerian is not very realistic for 67P, just for testing
-    ActorBuilder.set_orbit(sat1, [8000, 0, 0], [0.0, 2.0, 0.0], epoch, comet)
+    ActorBuilder.set_orbit(sat1, [4000, 1, 1], [0.0, 2.0, 0.0], epoch, comet)
 
     # Set the mesh and create some more satellites around 67P
     ActorBuilder.set_central_body(sat1, comet, (mesh_points, mesh_triangles))
 
     sat2 = ActorBuilder.get_actor_scaffold("sat2", SpacecraftActor, epoch=epoch)
-    ActorBuilder.set_orbit(sat2, [-8000, 1e-8, 1e-8], [0, -2.0, 0], epoch, comet)
+    ActorBuilder.set_orbit(sat2, [-4000, 1, 1], [2.0, 0, 0], epoch, comet)
     ActorBuilder.set_central_body(sat2, comet, (mesh_points, mesh_triangles))
 
     sat3 = ActorBuilder.get_actor_scaffold("sat3", SpacecraftActor, epoch=epoch)
-    ActorBuilder.set_orbit(sat3, [1e-8, 8000, 1e-8], [-2.0, 0, 0], epoch, comet)
+    ActorBuilder.set_orbit(sat3, [1, 4000, 1], [-2.0, 0, 0], epoch, comet)
     ActorBuilder.set_central_body(sat3, comet, (mesh_points, mesh_triangles))
 
     sat4 = ActorBuilder.get_actor_scaffold("sat4", SpacecraftActor, epoch=epoch)
-    ActorBuilder.set_orbit(sat4, [1e-8, -8000, 1e-8], [2.0, 0, 0], epoch, comet)
+    ActorBuilder.set_orbit(sat4, [1, -4000, 1], [0, -2.0, 0], epoch, comet)
     ActorBuilder.set_central_body(sat4, comet, (mesh_points, mesh_triangles))
+
+    sat5 = ActorBuilder.get_actor_scaffold("sat5", SpacecraftActor, epoch=epoch)
+    ActorBuilder.set_orbit(sat5, [1, 1, 4000], [2.0, 0, 0], epoch, comet)
+    ActorBuilder.set_central_body(sat5, comet, (mesh_points, mesh_triangles))
+
+    sat6 = ActorBuilder.get_actor_scaffold("sat6", SpacecraftActor, epoch=epoch)
+    ActorBuilder.set_orbit(sat6, [1, 1, -4000], [-2.0, 0, 0], epoch, comet)
+    ActorBuilder.set_central_body(sat6, comet, (mesh_points, mesh_triangles))
 
     # init simulation
     sim = paseos.init_sim(sat1, starting_epoch=epoch)
@@ -66,6 +74,8 @@ def test_mesh_for_central_body():
     sim.add_known_actor(sat2)
     sim.add_known_actor(sat3)
     sim.add_known_actor(sat4)
+    sim.add_known_actor(sat5)
+    sim.add_known_actor(sat6)
 
     # Write a plot to file, for debugging
     paseos.plot(sim, paseos.PlotType.SpacePlot, "mesh_test.png")
