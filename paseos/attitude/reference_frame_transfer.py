0242126e51ec97d435d507d4c29af78078388911
Mr-Medina <M.MedinaLeon@student.tudelft.nl>
2023-12-26 16:43:36 +0100

    added transformation matrix for RPY to body fixed frames

--- paseos/attitude/reference_frame_transfer.py
+++ paseos/attitude/reference_frame_transfer.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def transformation_matrix_eci_rpy(r, v):
     """
-    Creates the transformation matrix  to transform a vector in the Earth-Centered Inertial Frame (ECI) to the
+    Creates the transformation matrix to transform a vector in the Earth-Centered Inertial Frame (ECI) to the
     Roll-Pitch-Yaw (RPY) reference frame of the spacecraft (variant to Gaussian reference frame, useful for attitude
     disturbance modelling)
 
@@ -34,9 +34,55 @@ def transformation_matrix_eci_rpy(r, v):
 
     return T
 
-def eci_to_rpy(u, r, v):
+
+def transformation_matrix_rpy_body(euler_angles_in_rad):
+    """Creates the transformation matrix to transform a vector in the Roll-Pitch-Yaw (RPY) reference frame to the body
+    fixed reference frame of the spacecraft.
+
+    RPY reference frame:    x: along track direction
+                            y: -ĥ (angular momentum of the orbit). perpendicular to the orbital plane
+                            z: -r̂ (nadir)
+
+    body fixed frame:       when unperturbed, coincides with RPY frame. Perturbations result in non-zero roll pitch and
+                            yaw angles, rotating the body fixed frame.
+
+    To go from RPY to body fixed, this transformation matrix is used
+    To go from body fixed to RPY, the inverse is used.
+
+    Args:
+        euler_angles_in_rad (list of floats): [roll, pitch, yaw] in radians
+
+    Returns:
+        T (numpy array of floats): transformation matrix
     """
-    Converts a vector in the Earth-Centered Inertial Frame (ECI) to the Roll-Pitch-Yaw (RPY) reference frame of the
+    roll, pitch, yaw = euler_angles_in_rad
+
+    # individual axis rotations:
+    A = np.array([
+        [1, 0, 0],
+        [0, np.cos(roll), np.sin(roll)],
+        [0, -np.sin(roll), np.cos(roll)]
+    ])
+
+    B = np.array([
+        [np.cos(pitch), 0, -np.sin(pitch)],
+        [0, 1, 0],
+        [np.sin(pitch), 0, np.cos(pitch)]
+    ])
+
+    C = np.array([
+        [np.cos(yaw), np.sin(yaw), 0],
+        [-np.sin(yaw), np.cos(yaw), 0],
+        [0, 0, 1]
+    ])
+
+    # Transformation matrix:
+    T = A @ B @ C
+
+    return T
+
+def eci_to_rpy(u, r, v):
+    """Converts a vector in the Earth-Centered Inertial Frame (ECI) to the Roll-Pitch-Yaw (RPY) reference frame of the
     spacecraft, using transformation matrix from transformation_matrix_eci_rpy function.
 
     Args:
@@ -54,20 +100,20 @@ def eci_to_rpy(u, r, v):
     return T@u
 
 def rpy_to_eci(u, r, v):
+    """Converts a vector in the Roll-Pitch-Yaw (RPY) of the spacecraft to the Earth-Centered Inertial Frame (ECI)
+    reference frame, using transformation matrix from transformation_matrix_eci_rpy function.
+
+    Args:
+        u: vector in ECI
+        r: position vector of RPY reference frame wrt ECI frame
+        v: velocity of the spacecraft in earth reference frame, centered on spacecraft
+
+    Returns:
+        vector u w.r.t. ECI frame
     """
-        Converts a vector in the Roll-Pitch-Yaw (RPY) of the spacecraft to the Earth-Centered Inertial Frame (ECI)
-        reference frame, using transformation matrix from transformation_matrix_eci_rpy function.
-
-        Args:
-            u: vector in ECI
-            r: position vector of RPY reference frame wrt ECI frame
-            v: velocity of the spacecraft in earth reference frame, centered on spacecraft
-
-        Returns:
-            vector u w.r.t. ECI frame
-        """
 
     T = np.linalg.inv(transformation_matrix_eci_rpy(r, v))
 
     # transform u vector with matrix multiplication
     return T@u
+
