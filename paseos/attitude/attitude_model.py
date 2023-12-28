935ccc49dc04926c5e7c9d346d05b97fa58ef9f7
Mr-Medina <M.MedinaLeon@student.tudelft.nl>
2023-12-28 15:41:06 +0100

    disturbance torque in attitude model

--- paseos/attitude/attitude_model.py
+++ paseos/attitude/attitude_model.py
@@ -2,6 +2,7 @@ from loguru import logger
 import pykep as pk
 import numpy as np
 
+from disturbance_calculations import calculate_aero_torque, calculate_magnetic_torque, calculate_grav_torque
 
 class AttitudeModel:
 
@@ -29,6 +30,22 @@ class AttitudeModel:
         """
         u = np.array(self._actor.get_position)
         return -u/np.linalg.norm(u)
+
+    def disturbance_torque(self):
+        """Computes total torque due to user specified disturbances
+
+        Returns:
+            list [Tx, Ty, Tz]: total torques in Nm
+        """
+        T = np.array([0,0,0])
+        if "aerodynamic" in self._actor.get_disturbances:
+            T += calculate_aero_torque()
+        if "gravitational" in self._actor.get_disturbances:
+            T += calculate_grav_torque()
+        if "magnetic" in self._actor.get_disturbances:
+            T += calculate_magnetic_torque()
+        return T
+
 
     def update_attitude(self, dt):
         """
--- paseos/attitude/disturbance_calculations.py
+++ paseos/attitude/disturbance_calculations.py
@@ -1,25 +1,25 @@
 # this functions could be implemented inside the attitude model class but I'd rather have it
 # separately right now.
-
+import numpy as np
 
 def calculate_aero_torque():
 
     # calculations for torques
     # T must be in actor body fixed frame (to be discussed)
     T = [0, 0, 0]
-    return T
+    return np.array(T)
 
 
 def calculate_grav_torque():
     # calculations for torques
     # T must be in actor body fixed frame (to be discussed)
     T = [0, 0, 0]
-    return T
+    return np.array(T)
 
 
 def calculate_magnetic_torque():
     # calculations for torques
     # T must be in actor body fixed frame (to be discussed)
     T = [0, 0, 0]
-    return T
+    return np.array(T)
 
