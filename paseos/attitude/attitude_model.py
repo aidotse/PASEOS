b4080b327cd2f4619d333fa0cddabc0abf9f7213
Mr-Medina <M.MedinaLeon@student.tudelft.nl>
2023-12-29 14:58:56 +0100

    commit attitude model before changing most of it (to get dynamics correct)

--- paseos/attitude/attitude_model.py
+++ paseos/attitude/attitude_model.py
@@ -8,7 +8,8 @@ from paseos.attitude.disturbance_calculations import (calculate_aero_torque,
 from paseos.attitude.reference_frame_transfer import (eci_to_rpy,
                                                       rpy_to_eci,
                                                       rpy_to_body,
-                                                      body_to_rpy)
+                                                      body_to_rpy,
+                                                      get_euler)
 
 class AttitudeModel:
 
@@ -19,6 +20,8 @@ class AttitudeModel:
     _actor_angular_acceleration = None
     _actor_pointing_vector_rpy = None
     _actor_pointing_vector_eci = None
+
+    _actor_prev_pos = None
     def __init__(
             self,
             local_actor,
@@ -28,7 +31,9 @@ class AttitudeModel:
             actor_initial_angular_acceleration: list[float] = [0, 0, 0],
             # trial:
             actor_initial_pointing_vector_rpy: list[float] = [0,0,1],            # make this better
-            actor_initial_pointing_vector_eci: list[float] = None
+            actor_initial_pointing_vector_eci: list[float] = None,
+
+            actor_initial_previous_position = [1.e+07, 1.e-03, 1.e-03]
             ## add args with default value = None, if
             # actor_dipole
             # actor_drag_coefficient
@@ -43,6 +48,8 @@ class AttitudeModel:
         self._actor_pointing_vector_rpy = actor_initial_pointing_vector_rpy
         self._actor_pointing_vector_eci = actor_initial_pointing_vector_eci
 
+        self._actor_prev_pos = actor_initial_previous_position
+
 
     def nadir_vector(self):
         """computes unit vector pointing towards earth, inertial body frame
@@ -50,7 +57,7 @@ class AttitudeModel:
         Returns:
             np array ([x, y, z]): unit nadir vector in ECIF (Earth-centered inertial frame)
         """
-        u = np.array(self._actor.get_position)
+        u = np.array(self._actor.get_position(self._actor.local_time))
         return -u/np.linalg.norm(u)
 
     def calculate_disturbance_torque(self):
@@ -69,15 +76,15 @@ class AttitudeModel:
             T += calculate_magnetic_torque()
         return T
 
-    def move_pointing_vector(self):
+    def move_pointing_vector(self, pos, vel):
         self._actor_pointing_vector_eci = rpy_to_eci(self._actor_pointing_vector_rpy,
-                                                     self._actor.get_position,
-                                                     self._actor.get_position_velocity[1])
+                                                     pos,
+                                                     vel)
         self._actor_pointing_vector_eci = (self._actor_pointing_vector_eci +
-                                           self._actor.get_previous_position -
-                                           self._actor.get_position)
+                                           np.array(self._actor_prev_pos) -
+                                           pos)
 
-
+        #print(np.array(pos) - np.array(self._actor_prev_pos))
     def update_attitude(self, dt):
         """
 
@@ -87,9 +94,14 @@ class AttitudeModel:
         Returns:
             np array
         """
-        self.move_pointing_vector()
-        eci_to_rpy(self._actor_pointing_vector_eci, self._actor.get_position, self._actor.get_position_velocity[1])
-            # calculate angles between nedir and new pointing vector.
+        position = self._actor.get_position(self._actor.local_time)
+        velocity = self._actor.get_position_velocity(self._actor.local_time)[1]
+
+        self.move_pointing_vector(position, velocity)
+        self._actor_pointing_vector_rpy = eci_to_rpy(self._actor_pointing_vector_eci,
+                                                     position,
+                                                     velocity)
+        # calculate euler angles between nadir and new pointing vector.
 
         # constants:
         # self._actor_I =
@@ -114,7 +126,10 @@ class AttitudeModel:
         # velocity
         self._actor_angular_velocity += self._actor_angular_acceleration * dt
         # update attitude
-        self._actor_attitude_in_rad += self._actor_angular_velocity * dt
+        self._actor_attitude_in_rad += self._actor_angular_velocity * dt + get_euler(self._actor_pointing_vector_rpy,
+                                                                                     self.nadir_vector())
+        #print(get_euler(self._actor_pointing_vector_rpy, self.nadir_vector()))
         self._actor_attitude_in_rad = np.arctan2(
             np.sin(self._actor_attitude_in_rad), np.cos(self._actor_attitude_in_rad))
-        # out: new euler angles
+        # previous position (to be used in next computation)
+        self._actor_prev_pos = position
